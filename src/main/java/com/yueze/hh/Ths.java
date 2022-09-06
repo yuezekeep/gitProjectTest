60a7bcf92a66be57b659b347e8dcb2ebdd84f23a
yueze <2860339200@qq.com>
2022-09-06 21:31:55 +0800

    这是我的第一次提交Git测试

--- src/main/java/com/yueze/hh/Ths.java
+++ src/main/java/com/yueze/hh/Ths.java
@@ -3,5 +3,6 @@ package com.yueze.hh;
 public class Ths {
     public static void main(String[] args) {
         System.out.println("skgdgshjmgdjhagsjdgjsadgsjh测试用户");
+        System.out.println("第二个版本");
     }
 }
