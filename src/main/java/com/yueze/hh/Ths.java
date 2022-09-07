2cf852e3ce55b25a9dd9a75034cfa92399e5e4d2
yuezekeep <112839219+yuezekeep@users.noreply.github.com>
2022-09-07 11:01:29 +0800

    pull测试

--- src/main/java/com/yueze/hh/Ths.java
+++ src/main/java/com/yueze/hh/Ths.java
@@ -8,5 +8,6 @@ public class Ths {
         System.out.println("hot_fix分子");
         System.out.println("这是真正的hot_fix分支测试运行");
         System.out.println("push测试");
+        System.out.println("pull测试");
     }
 }
